15c7f77cf745f6584898007e9caf199d94f2d86d
andydeac <andy.deac16@gmail.com>
2020-12-28 22:43:14 +0200

    Implement Constants.cs class for API

---
+++ Messenger-API/Constants.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Messenger_API
+{
+    public class Constants
+    {
+        private IConfiguration configuration;
+
+        public Constants(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            Web_IP = configuration["Constants:Web_IP"];
+            Web_Port = configuration["Constants:Web_Port"];
+            Web_Protocol = configuration["Constants:Web_Protocol"];
+        }
+        public string Web_IP;
+        public string Web_Port;
+        public string Web_Protocol;
+
+        public string Web_App_Address
+        {
+            get => Web_Protocol + "://" + Web_IP + ":" + Web_Port;
+        }
+    }
+}
